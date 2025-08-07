76d843bf8070b93e44c8b344f714c0f8efc274d6
Ben Grier <grierben777@gmail.com>
2025-08-07 20:24:44 +0100

    Add initial script to calculate differences between cluster median ts

---
+++ src/ADRIA/HighResCoralStress/6_quantifying_cluster_differences.jl
@@ -0,0 +1,54 @@
+include("../../common.jl")
+
+context_layers = GDF.read(joinpath(output_path, "analysis_context_layers_carbonate.gpkg"))
+
+dhw_scenarios = open_dataset(joinpath(gbr_domain_path, "DHWs/dhwRCP45.nc"))
+GCMs = dhw_scenarios.dhw.properties["members"]
+
+gbr_dom = ADRIA.load_domain(gbr_domain_path, "45")
+areas = gbr_dom.loc_data.area
+
+all_levels = vec(collect(Iterators.product(GCMs, unique(context_layers.management_area))))
+distances = DataFrame(GCM = first.(all_levels), management_area = last.(all_levels), CID_cover = zeros(length(all_levels)), CID_dhw = zeros(length(all_levels)))
+
+for GCM in GCMs
+
+    absolute_cover = readcubedata(open_dataset(joinpath(output_path, "processed_model_outputs/median_cover_$(GCM).nc")).layer)
+    relative_cover = percentage_cover_timeseries(areas, absolute_cover)[1:50, :]
+
+    dhw_ts = gbr_dom.dhw_scens[1:50, :, i_gcm]
+    dhw_ts = rebuild(dhw_ts, dims=relative_cover.axes, metadata=dhw_timeseries_properties)
+
+    for man_area in unique(context_layers.management_area)
+        man_area_reefs = context_layers[context_layers.management_area .== man_area, :]
+
+        low_cluster_reefs = man_area_reefs[man_area_reefs[:, "$(GCM)_management_area_cluster_cats"] .== "low", :]
+        low_cluster_cover = relative_cover[locations=At(low_cluster_reefs.UNIQUE_ID)]
+        low_cluster_dhw = dhw_ts[locations=At(low_cluster_reefs.UNIQUE_ID)]
+
+        high_cluster_reefs = man_area_reefs[man_area_reefs[:, "$(GCM)_management_area_cluster_cats"] .== "high", :]
+        high_cluster_cover = relative_cover[locations=At(high_cluster_reefs.UNIQUE_ID)]
+        high_cluster_dhw = dhw_ts[locations=At(high_cluster_reefs.UNIQUE_ID)]
+
+        man_area_cid_cover = CID(
+            dropdims(median(low_cluster_cover, dims=2), dims=2),
+            dropdims(median(high_cluster_cover, dims=2), dims=2)
+        )
+        man_area_cid_dhw = CID(
+            dropdims(median(low_cluster_dhw, dims=2), dims=2),
+            dropdims(median(high_cluster_dhw, dims=2), dims=2)
+        )
+
+        distances[
+            (distances.GCM .== GCM) .& 
+            (distances.management_area .== man_area),
+        :CID_cover] .= man_area_cid_cover 
+
+        distances[
+            (distances.GCM .== GCM) .& 
+            (distances.management_area .== man_area),
+        :CID_dhw] .= man_area_cid_dhw
+    end
+end
+
+sort!(distances, [:management_area, :GCM])
